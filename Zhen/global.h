5e04b203148a5f1b61ef4acf4056688c14e4428d
WangTingZe <kingpin58@aliyun.com>
2026-01-11 22:30:38 +0800

    add compiler error message when no signals lib

--- Zhen/global.h
+++ Zhen/global.h
@@ -4,12 +4,14 @@
 #include <boost/signals2.hpp>
 #define USE_BOOST_ORG_LIB
 #define boost_ns boost
-#else
+#elif __has_include(<boost_signals2/signals2.hpp>)
 
-#if __has_include(<boost_signals2/signals2.hpp>)
 #include <boost_signals2/signals2.hpp>
 #define USE_SIGNAL2_ALONE_LIB
 #define boost_ns boist
-#endif
+
+#else
+
+#error "no boost signals header folder!"
 
 #endif
